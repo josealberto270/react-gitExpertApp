eee7ab01473b774f31d6ffd9333df13defdaa62d
Jose Alberto Zavala <alberto@perfilan.com>
2020-09-24 18:04:55 -0500

    docs agregado

---
+++ docs/precache-manifest.afcb557d1722b8db20c57b86d0c7ff58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582f7d26753fbdb23284e5f795f6a86f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76408849567f789dd948",
+    "url": "/static/css/main.3ab62ec1.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76408849567f789dd948",
+    "url": "/static/js/main.62a92b81.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
