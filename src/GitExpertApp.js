69a7a5db71e96a5f7935003c3c0a8d1ab234ddfa
Jose Alberto Zavala <alberto@perfilan.com>
2020-09-30 17:01:41 -0500

    prueba nps

--- src/GitExpertApp.js
+++ src/GitExpertApp.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { AddCategoria } from './components/AddCategoria'
 import { GifGrid } from './components/GifGrid'
+import '../src/static/nps'
 
 export const GifExpertApp = () => {
 
@@ -11,6 +12,16 @@ export const GifExpertApp = () => {
   //   const newCategoria = 'One Piece'
   //   setCategorias([...categorias,newCategoria])
   // }
+  useEffect(()=>{
+    delighted.survey({
+      email: "customer@hemandstitch.com", // customer email (optional)
+      name: "Bailey Dixon",               // customer name (optional)
+      properties: {                       // extra context (optional)
+        orderId: "123ABC",
+        shippingMethod: "Ground"
+      }
+    })
+  })
 
   return <>
     <h2>GifExpertApp</h2>
