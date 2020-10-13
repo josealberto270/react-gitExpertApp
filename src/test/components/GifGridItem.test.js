cc1b2011de0d167be181179e84cf14a17aca3dcc
Jose Alberto Zavala <alberto@perfilan.com>
2020-10-12 19:32:29 -0500

    enzyme

---
+++ src/test/components/GifGridItem.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import GifGridItem from '../../components/GifGridItem'
+
+describe('Pruebas en el componente GifGridItem', () => {
+
+  test('debe de mostrar <GifGridItem /> correctamente', () => {
+    
+    const wrapper = shallow( <GifGridItem /> )
+
+  })
+
+})
